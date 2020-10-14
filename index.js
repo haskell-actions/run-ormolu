f9be58ffc02bc174214d13546ce1f292c5e24243
Mark Karpov <markkarpov92@gmail.com>
2020-10-14 21:35:15 +0200

    Define inputs, do globbing for source files

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const tool_cache = require('@actions/tool-cache');
 const exec = require('@actions/exec');
+const glob = require('@actions/glob');
 
 const ormolu_linux_url = 'https://github.com/tweag/ormolu/releases/download/0.1.3.0/ormolu-Linux';
 const ormolu_windows_url = 'https://github.com/tweag/ormolu/releases/download/0.1.3.0/ormolu-Windows';
 const ormolu_macos_url = 'https://github.com/tweag/ormolu/releases/download/0.1.3.0/ormolu-macOS';
+const ormolu_version = '0.1.3.0';
+
+const input_pattern = core.getInput('pattern');
+const input_extra_args = core.getInput('extra-args');
 
 async function run() {
   try {
 
     // Download ormolu executable
 
-    let ormolu_path;
+    var ormolu_path;
 
     if (process.platform === 'win32') {
         ormolu_path = await tool_cache.downloadTool(ormolu_windows_url);
@@ -26,15 +31,20 @@ async function run() {
 
     // Cache ormolu executable
 
-    const ormolu_cached_path = await tool_cache.cacheFile(ormolu_path);
+    const ormolu_cached_path = await tool_cache.cacheFile(ormolu_path, 'ormolu', 'ormolu', ormolu_version);
 
     // Add ormolu to PATH
 
     core.addPath(ormolu_cached_path);
 
-    // TODO grep Haskell files and run ormolu
+    // Glob for the files to format
 
-    // TODO call git to highlight diffs
+    const globber = await glob.create(input_pattern);
+    const files = await globber.glob();
+
+    // // TODO grep Haskell files and run ormolu
+
+    // // TODO call git to highlight diffs
 
   } catch (error) {
     core.setFailed(error.message);
